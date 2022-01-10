41111a074d15f779f36d364bf931ba9bedd1af2c
Rahul Sharma <58264599+OrneyVortex@users.noreply.github.com>
2022-01-10 17:03:19 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+ 
+int main()
+{
+  int i, a = 50, count; 
+  
+
+  while(a <= 200)
+  {
+    count = 0;
+    i = 2;
+    while(i <= a/2)
+    {
+      if(a%i == 0)
+      {
+        count++;
+      }
+      i++;	
+    }	
+    if(count == 0)
+    {
+	printf(" %d \n", a);
+    }
+    a++; 
+  }
+  return 0;
+}
